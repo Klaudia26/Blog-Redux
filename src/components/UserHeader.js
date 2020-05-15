e05c20d2a8f0caa53f312f42d47ad293c46e2c03
Klaudia <klaudia.krzywda@gmail.com>
2020-05-15 10:38:05 +0100

    blog-redux

---
+++ src/components/UserHeader.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { fetchUser } from '../components/actions';
+
+class UserHeader extends Component {
+  // componentDidMount() {
+  //   this.props.fetchUser(this.props.userId);
+  // }
+  render() {
+    // const user = this.props.user.find((user) => user.id === this.props.userId);
+
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user.find((user) => user.id === ownProps.userId),
+  };
+};
+
+export default connect(mapStateToProps)(UserHeader);
